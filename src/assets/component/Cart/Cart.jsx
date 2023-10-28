0ed50afbf512f9635ba697bdfcaf471a7e4b12f4
Md.khosruzzaman <khosru.skz@gmail.com>
2023-10-28 15:23:32 +0100

    added cart component

---
+++ src/assets/component/Cart/Cart.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Cart.css";
+
+const Cart = (props) => {
+  // const cart = props.cart;//optio one
+
+  const { cart } = props;
+  let totalPrice = 0;
+  let totalShipping = 0;
+
+  for (const product of cart) {
+    totalPrice = totalPrice + product.price;
+    totalShipping = totalShipping + product.shipping;
+  }
+  const tax = (totalPrice * 7) / 100;
+  const grandTotal = totalPrice + totalShipping + tax;
+
+  return (
+    <div className="cart">
+      <h4>Order summery</h4>
+      <p>Selected Items: {cart.length}</p>
+      <p>Total price :</p>
+      <p>Total Shipping:{totalShipping} </p>
+      <p> Tax : {tax.toFixed(2)} </p>
+      <h6>Grand Total:{grandTotal} </h6>
+    </div>
+  );
+};
+
+export default Cart;
